b53665d0e12a6fce7be3eb1ddb5ee8bd4bba9704
unknown <gadalai426@gmail.com>
2023-11-01 21:25:22 +0800

    gitignore

--- utils/Mail.js
+++ utils/Mail.js
@@ -1,1 +1,16 @@
 const nodemailer =require("nodemailer");
+
+
+const sendMail= async ()=>{
+
+    const transporter=nodemailer.createTransport({
+        service:"gmail",
+        port:587,
+        secure: false,
+        auth: {
+          user: "unshy.ebook@gmail.com",
+          pass: process.env.NODE_MAILER_SECRET,
+        },
+
+    });
+}
